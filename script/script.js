c663e3a5db6fb6f00e5e5fbb3bcbadcd79276e7e
starshipgoesby <zhafranuarrybawafi@gmail.com>
2024-02-04 23:54:28 +0700

    final-project

---
+++ script/script.js
@@ -0,0 +1,7 @@
+openPopup = () => {
+    document.getElementById("orderPopup").style.display = "block";
+}
+
+closePopup = () => {
+    document.getElementById("orderPopup").style.display = "none";
+}
